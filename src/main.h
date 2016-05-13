85aa36359b9de7be8932de5060293ddebb1b6f9b
Braydon Fuller <braydon@bitpay.com>
2016-05-13 11:43:01 -0400

    main: add amount and address to spentindex value

--- src/main.h
+++ src/main.h
@@ -303,6 +303,9 @@ struct CSpentIndexValue {
     uint256 txid;
     unsigned int inputIndex;
     int blockHeight;
+    CAmount satoshis;
+    int addressType;
+    uint160 addressHash;
 
     ADD_SERIALIZE_METHODS;
 
@@ -311,12 +314,18 @@ struct CSpentIndexValue {
         READWRITE(txid);
         READWRITE(inputIndex);
         READWRITE(blockHeight);
+        READWRITE(satoshis);
+        READWRITE(addressType);
+        READWRITE(addressHash);
     }
 
-    CSpentIndexValue(uint256 t, unsigned int i, int h) {
+    CSpentIndexValue(uint256 t, unsigned int i, int h, CAmount s, int type, uint160 a) {
         txid = t;
         inputIndex = i;
         blockHeight = h;
+        satoshis = s;
+        addressType = type;
+        addressHash = a;
     }
 
     CSpentIndexValue() {
@@ -327,6 +336,9 @@ struct CSpentIndexValue {
         txid.SetNull();
         inputIndex = 0;
         blockHeight = 0;
+        satoshis = 0;
+        addressType = 0;
+        addressHash.SetNull();
     }
 
     bool IsNull() const {
